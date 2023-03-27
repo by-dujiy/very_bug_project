c9512a677a725aa5a915fff973aa3e17015fd6dd
bydujiy <bydujiy@gmail.com>
2023-03-27 15:38:55 +0300

    create simple api

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
 from app import app
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, jsonify
 from report_of_monaco_2018_racing_bydujiy import main as rr
+from flask_restful import Api, Resource
+from flasgger import Swagger, swag_from
+import json
 
 
-drive_data, start_data, end_data = rr.read_logs('app/static/data_files')
+api = Api(app)
+swag = Swagger(app)
+
+
+drive_data, start_data, end_data = rr.read_logs('static/data_files')
 drivers_list = rr.get_drivers(drive_data)
 race_report = rr.get_report(drive_data, start_data, end_data)
 
@@ -33,7 +40,33 @@ def print_driver():
     return redirect(url_for('driver', driver_id=driver_id))
 
 
-@app.route('/report/driver/<string:driver_id>')
+@app.route('/report/drivers/<string:driver_id>')
 def driver(driver_id):
     driver_report = rr.search_by_abbr(race_report, driver_id)
     return render_template('report/driver.html', driver_report=driver_report)
+
+
+class Report(Resource):
+    @swag_from('docs/racer_report.yml')
+    def get(self):
+        # dict_report = [
+        #     {
+        #         'abbr': item[0],
+        #         'name': item[1],
+        #         'team': item[2],
+        #         'time': item[3]
+        #     } for item in race_report
+        # ]
+        # json_report = json.dumps(dict_report, indent=2)
+        # print(json_report)
+        return jsonify(race_report)
+
+
+class Drivers(Resource):
+    @swag_from('docs/drivers.yml')
+    def get(self):
+        return jsonify(drivers_list)
+
+
+api.add_resource(Report, '/api/racer-report')
+api.add_resource(Drivers, '/api/drivers')
